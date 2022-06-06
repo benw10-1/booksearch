0762b23ac6f1606eff4b0073da1f527c507a3b86
Ben Wirth <benjaminwirth10@gmail.com>
2022-06-05 18:22:21 -0700

    file structure for backend

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
 const { authMiddleware } = require('./utils/auth');
+
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
